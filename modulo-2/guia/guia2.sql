73459cf627c5db9a69875d2955ca6c51d09b1da4
estebanmatias92 <estebanmatias92@gmail.com>
2023-05-28 21:34:11 -0300

    Added constraints to guia2.sql

--- modulo-2/guia/guia2.sql
+++ modulo-2/guia/guia2.sql
@@ -16,7 +16,7 @@ CREATE TABLE Orders (
     OrderID INTEGER PRIMARY KEY AUTOINCREMENT,
     EmployeeID INTEGER NOT NULL,
     Product TEXT NOT NULL UNIQUE,
-    FOREIGN KEY(EmployeeID) REFERENCES Employees(EmployeeID)
+    FOREIGN KEY (EmployeeID) REFERENCES Employees(EmployeeID)
 );
 
 -- Insertar Datos en Employees
